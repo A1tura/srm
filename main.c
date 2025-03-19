a828c0621569e3aa7b82a5c6251a83c0da50a283
A1tura <plandik275@gmail.com>
2025-03-19 16:22:09 +0100

    init

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int overwrite(int t_fd, int r_fd, struct stat t_file) {
+  int buffer[t_file.st_size];
+
+  read(r_fd, &buffer, t_file.st_size);
+  write(t_fd, buffer, t_file.st_size);
+
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Now enought arguments");
+    return -1;
+  }
+
+  struct stat t_fileStats;
+  int fd = open("/dev/random", O_RDONLY);
+  int fd_t = open(argv[1], O_WRONLY, 0644);
+
+  if (fd == -1) {
+    printf("Error while reading /dev/zero");
+    return 1;
+  }
+  if (fd_t == -1) {
+    printf("Error while reading target file");
+    return 1;
+  }
+  if (stat(argv[1], &t_fileStats) == -1) {
+    printf("Error while getting file info");
+    return 1;
+  }
+
+  for (int i = 0; i < 5; i++) {
+    overwrite(fd_t, fd, t_fileStats);
+  }
+
+  close(fd);
+  close(fd_t);
+
+  if (remove(argv[1]) == -1) {
+    printf("Error while deleting file");
+    return 1;
+  }
+
+  return 0;
+}
